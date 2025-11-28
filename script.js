209adfa6420093fd12d5f7784a16d2babab7feb4
nik-100xdevz <rainikhil760@gmail.com>
2025-11-28 11:44:46 +0530

    change in style

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 const lenis = new Lenis();
-
 function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
+  lenis.raf(time);
+  requestAnimationFrame(raf);
 }
 requestAnimationFrame(raf);
+
 gsap.registerPlugin(ScrollTrigger);
 
-document.querySelectorAll(".elem").forEach(elem =>{
-    let image = elem.querySelector("img")
-    let tl = gsap.timeline()
+document.querySelectorAll(".elem").forEach((elem) => {
+  const image = elem.querySelector("img");
 
-    let xTransform = gsap.utils.random(-100, 100)
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: elem,
+      start: "top bottom",
+      end: "bottom top",
+      scrub: 1.5,
+    }
+  });
 
-    tl
-        .set(image,{
-            transformOrigin:`${xTransform <0 ? 0 : '100%' }`,
-        },"start")
-    
-    .to(image,{
-        scale:0,
-        ease:"none",
-        scrollTrigger: {
-            trigger: image,
-            start:"top top",
-            end: "bottom top",
-            scrub:true
-        }
-    },"start")
-    .to(elem,{
-        xPercent: xTransform,
-        ease:"Power4.easeOut",
-        scrollTrigger: {
-            trigger: image,
-            start:"top bottom",
-            end: "bottom top",
-            scrub:true
-        }
-    },"start")
-})
+  const randomX = gsap.utils.random(-120, 120);
+  const randomRot = gsap.utils.random(-15, 15);
+  const randomZ = gsap.utils.random(-150, -300);
+
+  tl.set(elem, {
+    transformPerspective: 800,
+    transformStyle: "preserve-3d",
+  });
+
+  tl.to(elem, {
+    xPercent: randomX,
+    rotateZ: randomRot,
+    rotateX: gsap.utils.random(-10, 10),
+    z: randomZ,
+    ease: "expo.out",
+  }, 0);
+
+  tl.to(image, {
+    scale: 0.3,
+    opacity: 0,
+    rotateZ: -randomRot,
+    filter: "blur(px)",
+    ease: "power2.out",
+  }, 0);
+});
